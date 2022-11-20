a1e7d542bddd204f349454190929571ba847a158
test <test@neto.com>
2022-11-20 13:10:37 +0300

    первый комит

---
+++ Trening_parsing.py
@@ -0,0 +1,18 @@
+import requests
+url = 'https://netology.ru/blog/'
+response = requests.get(url)
+
+from pprint import pprint
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(response.text, 'html.parser')
+list = soup.find_all('div', class_='posts__item')
+
+
+for res in list:
+    title = res.find('a', class_='posts__link').text
+    print(title)
+
+
+
+
